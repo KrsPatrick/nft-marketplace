7f306e310ff91954fcbd1d3fe975af656fd1bb8a
KrsPatrick <lesiewicz.patrick@gmail.com>
2022-08-15 18:22:50 +0200

    added mock nfts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Example from "../components/navbar/navbar.components"
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Example />
     <Component {...pageProps} />
     </>
   ) 
